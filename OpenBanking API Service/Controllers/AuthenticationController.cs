16cdb7e30c984d9813a16f57662c53268e070c8e
Promise30 <promiseadeagbo@gmail.com>
2023-12-28 12:06:47 +0100

    Add project files.

---
+++ OpenBanking API Service/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenBanking_API_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
