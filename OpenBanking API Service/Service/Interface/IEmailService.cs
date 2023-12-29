c56096d7d60901f03d1164ee123a8a431b1ef802
Promise30 <promiseadeagbo@gmail.com>
2023-12-29 14:05:25 +0100

    Modified method return type

---
+++ OpenBanking API Service/Service/Interface/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using OpenBanking_API_Service_Common.Library.Models;
+
+namespace OpenBanking_API_Service.Service.Interface
+{
+    public interface IEmailService
+    {
+        string SendEmail(EmailMessage message);
+    }
+}
