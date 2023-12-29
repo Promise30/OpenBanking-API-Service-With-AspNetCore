6d6bb6fa13c295ffc6ad0649340e2d3bd9a6f1bf
Promise30 <promiseadeagbo@gmail.com>
2023-12-29 14:06:30 +0100

    Added various method definitions for user service

---
+++ OpenBanking API Service/Service/Interface/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OpenBanking_API_Service.Dtos;
+using OpenBanking_API_Service_Common.Library.Models;
+
+namespace OpenBanking_API_Service.Service.Interface
+{
+    public interface IUserService
+    {
+        Task<APIResponse<object>> CreateUserWithToken(RegisterUserDto registerUser);
+        Task<APIResponse<LoginResponse>> GetJwtTokenAsync(IdentityUser user);
+        Task<APIResponse<LoginOtpResponse>> GetOtpByLoginAsync(LoginDto loginDto);
+        Task<APIResponse<LoginResponse>> LogInUserWIthOtpAsync(string otp, string email);
+    }
+}
