8744b7c237f7f953360b45ad518ac499f1f6c433
Promise30 <promiseadeagbo@gmail.com>
2023-12-29 14:05:46 +0100

    Extended SendEmail method implementation

---
+++ OpenBanking API Service/Service/Implementation/EmailService.cs
@@ -0,0 +1,63 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using OpenBanking_API_Service.Service.Interface;
+using OpenBanking_API_Service_Common.Library.Constants;
+using OpenBanking_API_Service_Common.Library.Models;
+
+namespace OpenBanking_API_Service.Service.Implementation
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailConfiguration _emailConfiguration;
+        public EmailService(EmailConfiguration emailConfiguration)
+        {
+            _emailConfiguration = emailConfiguration;
+        }
+
+        public string SendEmail(EmailMessage message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+            var recipients = string.Join(", ", message.To);
+            return ResponseMessages.GetEmailSuccessMessage(recipients);
+
+
+        }
+        #region Private methods
+        private MimeMessage CreateEmailMessage(EmailMessage message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("email", _emailConfiguration.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Body };
+
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using var client = new SmtpClient();
+            try
+            {
+                client.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(_emailConfiguration.UserName, _emailConfiguration.Password);
+
+                client.Send(mailMessage);
+            }
+            catch
+            {
+                //log an error message or throw an exception or both.
+                throw;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+        }
+        #endregion
+
+    }
+}
