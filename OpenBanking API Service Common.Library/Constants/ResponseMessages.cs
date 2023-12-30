7d8dcab03b21562cb3814e0382bef0d53890f97f
Promise30 <promiseadeagbo@gmail.com>
2023-12-30 23:18:03 +0100

    Project constants

---
+++ OpenBanking API Service Common.Library/Constants/ResponseMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenBanking_API_Service_Common.Library.Constants
+{
+    public static class ResponseMessages
+    {
+        public static string GetEmailSuccessMessage(string emailAddress) => $"Email sent successfully to {emailAddress}";
+    }
+}
