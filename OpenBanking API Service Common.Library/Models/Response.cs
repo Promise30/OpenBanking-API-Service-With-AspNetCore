eecf493ee7fa2834ffc99abbd4561396259fb4f8
Promise30 <promiseadeagbo@gmail.com>
2023-12-30 23:17:32 +0100

    Initial API response model

---
+++ OpenBanking API Service Common.Library/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenBanking_API_Service_Common.Library.Models
+{
+    public class Response
+    {
+        //public string? Status { get; set; }
+        public string? Message { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
