ab1b5a48db21c9721dbaa80daa4c485652da7672
Promise30 <promiseadeagbo@gmail.com>
2023-12-31 23:20:40 +0100

    Initial model for IdentityUser extension

---
+++ OpenBanking API Service/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using OpenBanking_API_Service_Common.Library.Entities.Account;
+
+
+namespace OpenBanking_API_Service.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTimeOffset CreatedAt { get; set; }
+        public virtual BankAccount BankAccount { get; set; }
+    }
+}
