a59d5aa8d6594b7a3122c554d5ba89f91fed04f6
Promise30 <promiseadeagbo@gmail.com>
2023-12-31 23:20:02 +0100

    Initial model for login Otp response

--- OpenBanking API Service/Dtos/LoginOtpResponse.cs
+++ OpenBanking API Service/Dtos/LoginOtpResponse.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using OpenBanking_API_Service.Data;
 
 namespace OpenBanking_API_Service.Dtos
 {
     public class LoginOtpResponse
     {
         public string Token { get; set; }
-        public IdentityUser User { get; set; }
+        public ApplicationUser User { get; set; }
         public bool IsTwoFactorEnabled { get; set; }
     }
 }
