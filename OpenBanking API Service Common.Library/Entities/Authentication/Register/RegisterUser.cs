53a6bf4629580263d8d9816464c207f78801aa0c
Promise30 <promiseadeagbo@gmail.com>
2023-12-31 23:21:05 +0100

    Initial model for user registration

---
+++ OpenBanking API Service Common.Library/Entities/Authentication/Register/RegisterUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenBanking_API_Service_Common.Library.Entities.Authentication.Register
+{
+    public class RegisterUser
+    {
+
+        [Required(ErrorMessage = "Username field is required")]
+        public string UserName { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "Email field is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password field is required")]
+        [MinLength(8, ErrorMessage = "Password cannot be less than 8 characters")]
+        public string Password { get; set; }
+    }
+}
