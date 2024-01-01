96751c4cd78159ffc2c18b3b773ab22d29e1b229
Promise30 <promiseadeagbo@gmail.com>
2024-01-01 18:32:28 +0100

    task: Added models for Withdrawal request and response

---
+++ OpenBanking API Service/Dtos/AccountsDto/CreateBankAccountWithdrawal.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenBanking_API_Service.Dtos.AccountsDto
+{
+    public class CreateBankAccountWithdrawal
+    {
+        public double Amount { get; set; }
+        public int Pin { get; set; }
+
+    }
+}
