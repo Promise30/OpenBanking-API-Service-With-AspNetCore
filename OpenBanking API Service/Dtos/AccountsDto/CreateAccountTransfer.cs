4690cb772422cfb4afdedea7ff7670555bb586d2
Promise30 <promiseadeagbo@gmail.com>
2024-01-01 19:27:08 +0100

    task: account transfer request and response model

---
+++ OpenBanking API Service/Dtos/AccountsDto/CreateAccountTransfer.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenBanking_API_Service.Dtos.AccountsDto
+{
+    public class CreateAccountTransfer
+    {
+        public string SourceAccount { get; set; }
+        public double Amount { get; set; }
+        public string Narration { get; set; }
+        public string DestinationAccount { get; set; }
+        public int Pin { get; set; }
+    }
+}
---
+++ OpenBanking API Service/Dtos/AccountsDto/CreateAccountTransferResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenBanking_API_Service.Dtos.AccountsDto
+{
+    public class CreateAccountTransferResponse
+    {
+        public string SourceAccount { get; set; }
+        public double Amount { get; set; }
+        public double Balance { get; set; }
+        public string Narration { get; set; }
+        public string DestinationAccount { get; set; }
+        public DateTimeOffset TransactionDate { get; set; }
+    }
+}
