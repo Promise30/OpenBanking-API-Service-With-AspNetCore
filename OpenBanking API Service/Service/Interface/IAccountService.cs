4c2eca4377295deace2c0e7e786cae93a9b271b1
Promise30 <promiseadeagbo@gmail.com>
2024-01-01 16:31:05 +0100

    task: createAccount and depositAmount definition

---
+++ OpenBanking API Service/Service/Interface/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using OpenBanking_API_Service.Dtos.AccountsDto;
+using OpenBanking_API_Service_Common.Library.Models;
+
+namespace OpenBanking_API_Service.Service.Interface
+{
+    public interface IAccountService
+    {
+        Task<APIResponse<CreateAccountResponse>> CreateBankAccount(CreateBankAccountDto createBankAccountDto);
+        Task<APIResponse<CreateBankDepositResponse>> BankAccountDeposit(string accountNumber, CreateBankAccountDepositDto bankAccountDepositDto);
+    }
+}
