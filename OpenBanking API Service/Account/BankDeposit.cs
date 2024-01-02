b22bf96a6a270acf9a2ebf18d5abd91a00ff2885
Promise30 <promiseadeagbo@gmail.com>
2024-01-02 23:23:41 +0100

    task: initial model for BankDeposit

---
+++ OpenBanking API Service/Account/BankDeposit.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenBanking_API_Service_Common.Library.Entities.Account
+{
+    public class BankDeposit
+    {
+        public Guid Id { get; set; }
+        public string AccountNumber { get; set; }
+        public double Amount { get; set; }
+
+        public double Balance { get; set; }
+        public DateTimeOffset TransactionDate { get; set; }
+        public Guid BankAccountId { get; set; }
+        public virtual BankAccount BankAccount { get; set; }
+    }
+}
