8507429fe24b7b72fb31c85aed51c72cd2b66811
Promise30 <promiseadeagbo@gmail.com>
2024-01-03 16:22:15 +0100

    task: initial model with properties for all account related transaction types

---
+++ OpenBanking API Service/Dtos/AccountsDto/TransactionHistoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenBanking_API_Service.Dtos.AccountsDto
+{
+    public class TransactionHistoryDto
+    {
+        public IEnumerable<CreateBankDepositResponse> BankDeposits { get; set; }
+        public IEnumerable<AccountWithdrawalResponse> BankWithdrawals { get; set; }
+        public IEnumerable<CreateAccountTransferResponse> BankTransfers { get; set; }
+    }
+}
