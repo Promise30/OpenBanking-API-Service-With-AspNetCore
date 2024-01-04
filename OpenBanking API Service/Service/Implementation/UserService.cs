35f7efe5ad095bf1926d6c38d6d9eb4de69a0b39
Promise30 <promiseadeagbo@gmail.com>
2024-01-04 17:48:10 +0100

    task: implemented reset password method, emailConfirmation method and modified existing methods

--- OpenBanking API Service/Service/Implementation/UserService.cs
+++ OpenBanking API Service/Service/Implementation/UserService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using OpenBanking_API_Service.Data;
 using OpenBanking_API_Service.Dtos;
@@ -16,37 +17,61 @@ namespace OpenBanking_API_Service.Service.Implementation
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailService _emailService;
         private readonly ILogger<UserService> _logger;
         private readonly IConfiguration _configuration;
 
         public UserService(UserManager<ApplicationUser> userManager,
                             RoleManager<IdentityRole> roleManager,
                             SignInManager<ApplicationUser> signInManager,
+                            IEmailService emailService,
                             ILogger<UserService> logger,
                             IConfiguration configuration)
         {
             _logger = logger;
             _userManager = userManager;
             _roleManager = roleManager;
+            _emailService = emailService;
             _signInManager = signInManager;
             _configuration = configuration;
         }
-        public async Task<APIResponse<object>> CreateUserWithToken(RegisterUserDto registerUser)
+
+        public async Task<APIResponse<object>> UserEmailConfirmation(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return APIResponse<object>.Create(HttpStatusCode.NotFound, "User does not exist", null);
+
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+            if (result.Succeeded)
+            {
+                return APIResponse<object>.Create(HttpStatusCode.OK, "User email verification successful", null);
+            }
+            return APIResponse<object>.Create(HttpStatusCode.BadRequest, "User email verification failed.", null);
+        }
+
+        public async Task<APIResponse<object>> RegisterNewUser(RegisterUserDto registerUser)
         {
             try
             {
+
                 // Check if user already exists
                 var userExists = await _userManager.FindByEmailAsync(registerUser.Email);
                 if (userExists != null)
                 {
                     return APIResponse<object>.Create(HttpStatusCode.UnprocessableEntity, "User already exists", null);
                 }
+                // Create a new User
                 ApplicationUser user = new ApplicationUser
                 {
                     Email = registerUser.Email,
                     UserName = registerUser.UserName,
                     SecurityStamp = Guid.NewGuid().ToString(),
-                    TwoFactorEnabled = true
+                    TwoFactorEnabled = true,
+                    CreatedAt = DateTimeOffset.UtcNow.LocalDateTime,
+                    LockoutEnd = DateTimeOffset.UtcNow.LocalDateTime
                 };
 
                 _logger.LogInformation("Creating new user...");
@@ -57,12 +82,16 @@ namespace OpenBanking_API_Service.Service.Implementation
                 {
                     await _userManager.AddToRoleAsync(user, "User");
 
-                    _logger.LogInformation("User {@result} created successfully and role added", result);
+                    _logger.LogInformation("User with username {@user} created successfully and role assigned.", user.UserName);
 
                     var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    return APIResponse<object>.Create(HttpStatusCode.OK, "User created successfully. An email confirmation token has been sent to your mail.", token);
+
+                    _logger.LogInformation("Email confirmation token generated for new user");
+
+                    return APIResponse<object>.Create(HttpStatusCode.OK, "User created successfully. An email confirmation token has been sent to your registered email.", token);
 
                 }
+
                 return APIResponse<object>.Create(HttpStatusCode.BadRequest, "User registration unsuccessful", null);
 
             }
@@ -76,10 +105,11 @@ namespace OpenBanking_API_Service.Service.Implementation
         public async Task<APIResponse<LoginResponse>> GetJwtTokenAsync(ApplicationUser user)
         {
             var authClaims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, user.UserName),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            };
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
             var userRoles = await _userManager.GetRolesAsync(user);
             foreach (var role in userRoles)
             {
@@ -87,55 +117,88 @@ namespace OpenBanking_API_Service.Service.Implementation
             }
 
             var jwtToken = GetToken(authClaims);
-            return new APIResponse<LoginResponse>(HttpStatusCode.OK, "User access token generated successfully.", new LoginResponse() { AccessToken = new TokenType() { Token = new JwtSecurityTokenHandler().WriteToken(jwtToken), TokenExpiryDate = jwtToken.ValidTo } });
+            return APIResponse<LoginResponse>.Create(HttpStatusCode.OK, "User access token generated successfully.", new LoginResponse { Token = new JwtSecurityTokenHandler().WriteToken(jwtToken), TokenExpiryDate = jwtToken.ValidTo });
         }
 
-        public async Task<APIResponse<LoginOtpResponse>> GetOtpByLoginAsync(LoginDto loginDto)
+        public async Task<APIResponse<LoginOtpResponse>> LoginUserAsync(LoginDto loginDto)
         {
             var user = await _userManager.FindByEmailAsync(loginDto.Email);
 
-            if (user != null)
+            if (user == null)
             {
-                // Optional: Add logging for debugging
-                _logger.LogInformation($"User found: {user.UserName}");
+                return APIResponse<LoginOtpResponse>.Create(HttpStatusCode.BadRequest, "Invalid login credentials", null);
+            }
+            await _signInManager.SignOutAsync();
 
-                await _signInManager.SignOutAsync();
-
-                await _signInManager.PasswordSignInAsync(user, loginDto.Password, false, true);
-
-
-                if (user.TwoFactorEnabled)
-                {
-                    var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
-                    return new APIResponse<LoginOtpResponse>(HttpStatusCode.OK, "An OTP has been sent to your email.", new LoginOtpResponse { Token = token, User = user, IsTwoFactorEnabled = user.TwoFactorEnabled });
-                }
-
-                return new APIResponse<LoginOtpResponse>(HttpStatusCode.OK, "2FA is not enabled.", new LoginOtpResponse { Token = string.Empty, User = user, IsTwoFactorEnabled = user.TwoFactorEnabled });
+            var result = await _signInManager.PasswordSignInAsync(user, loginDto.Password, loginDto.RememberMe, false);
 
 
-                //// Optional: Add logging for debugging
-                //_logger.LogWarning($"Sign in failed for user: {user.UserName}");
+            if (result.RequiresTwoFactor)
+            {
 
-                //return new APIResponse<LoginOtpResponse>(HttpStatusCode.Unauthorized, "Invalid credentials.", null);
+                var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+                return APIResponse<LoginOtpResponse>.Create(HttpStatusCode.OK, "An OTP code has been generated and sent to your email", new LoginOtpResponse { Token = token });
+
+            }
+            if (!result.Succeeded)
+            {
+                // Handle the case where the password is incorrect
+                return APIResponse<LoginOtpResponse>.Create(HttpStatusCode.BadRequest, "Incorrect password", null);
             }
 
-            return new APIResponse<LoginOtpResponse>(HttpStatusCode.NotFound, "User does not exist.", null);
-
+            return APIResponse<LoginOtpResponse>.Create(HttpStatusCode.BadRequest, "Two-factor authentication is not enabled for this user", null);
         }
 
-        public async Task<APIResponse<LoginResponse>> LogInUserWIthOtpAsync(string otp, string email)
+        public async Task<APIResponse<LoginResponse>> LoginUserWithTwoFactorEnabled(TwoFactorModel twoFactorModel)
         {
-            var user = await _userManager.FindByEmailAsync(email);
-            var signIn = await _signInManager.TwoFactorSignInAsync("Email", otp, false, false);
-            if (signIn.Succeeded)
+            var user = await _userManager.FindByEmailAsync(twoFactorModel.Email);
+            //var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null)
             {
-                if (user != null)
-                {
-                    return await GetJwtTokenAsync(user);
-                }
+                return APIResponse<LoginResponse>.Create(HttpStatusCode.BadRequest, "User does not exist", null);
+            }
+            //await _signInManager.SignOutAsync();
+            var result = await _signInManager.TwoFactorSignInAsync("Email", twoFactorModel.OTP, twoFactorModel.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return await GetJwtTokenAsync(user);
+            }
+            if (result.IsLockedOut)
+            {
+                return APIResponse<LoginResponse>.Create(HttpStatusCode.BadRequest, "Account has been locked out.", null);
             }
             return new APIResponse<LoginResponse>(HttpStatusCode.BadRequest, "Login process unsuccessful", null);
         }
+        public async Task<APIResponse<object>> ForgotPasswordRequest(ForgotPassword forgotPassword)
+        {
+            var user = await _userManager.FindByEmailAsync(forgotPassword.Email);
+            if (user != null)
+            {
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                return APIResponse<object>.Create(HttpStatusCode.OK, "Password reset token generated successfully", token);
+
+            }
+            return APIResponse<object>.Create(HttpStatusCode.BadRequest, "Request unsuccessful", null);
+        }
+
+        public async Task<APIResponse<object>> PasswordResetAsync(ResetPassword resetPassword)
+        {
+            var user = await _userManager.FindByEmailAsync(resetPassword.Email);
+            if (user == null)
+            {
+                return APIResponse<object>.Create(HttpStatusCode.BadRequest, "User does not exist", null);
+
+            }
+            var resetPasswordResult = await _userManager.ResetPasswordAsync(user, resetPassword.Token, resetPassword.Password);
+            if (resetPasswordResult.Succeeded)
+            {
+                return APIResponse<object>.Create(HttpStatusCode.OK, "Password successfully changed.", null);
+            }
+            return APIResponse<object>.Create(HttpStatusCode.BadRequest, "Password change unsuccessful.", null);
+        }
+
+
+
 
         #region PrivateMethods
         private JwtSecurityToken GetToken(List<Claim> authClaims)
@@ -155,6 +218,7 @@ namespace OpenBanking_API_Service.Service.Implementation
                 );
             return token;
         }
+
 
         #endregion
     }
