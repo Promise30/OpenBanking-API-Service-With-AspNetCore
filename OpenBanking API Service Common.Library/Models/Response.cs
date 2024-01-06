5daf43b583eb38a7e53d192493490e1d329495e6
Promise30 <promiseadeagbo@gmail.com>
2024-01-06 22:16:21 +0100

    task: moved files to main project namespace

--- OpenBanking API Service Common.Library/Models/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpenBanking_API_Service_Common.Library.Models
-{
-    public class Response
-    {
-        //public string? Status { get; set; }
-        public string? Message { get; set; }
-        public bool IsSuccess { get; set; }
-    }
-}
