3339a917c5cfa7c333994f1f1efc5dd290933827
Promise30 <promiseadeagbo@gmail.com>
2024-01-07 12:36:29 +0100

    task: changed DateOnly property for date of birth to DateTimeOffSet

--- OpenBanking API Service/Dtos/AccountsDto/CreateBankAccountDto.cs
+++ OpenBanking API Service/Dtos/AccountsDto/CreateBankAccountDto.cs
@@ -5,7 +5,7 @@
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Middlename { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
         public string StateOfOrigin { get; set; }
--- OpenBanking API Service/Entities/Account/BankAccount.cs
+++ OpenBanking API Service/Entities/Account/BankAccount.cs
@@ -10,7 +10,7 @@ namespace OpenBanking_API_Service.Entities.Account
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Middlename { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
         public string StateOfOrigin { get; set; }
