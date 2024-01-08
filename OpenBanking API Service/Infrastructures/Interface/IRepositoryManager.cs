0dd8adfd1e1864abed53789da5c2561fce47db33
Promise30 <promiseadeagbo@gmail.com>
2024-01-08 21:30:05 +0100

    task: initial repository manager interface declaration

---
+++ OpenBanking API Service/Infrastructures/Interface/IRepositoryManager.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenBanking_API_Service.Infrastructures.Interface
+{
+    public interface IRepositoryManager
+    {
+        IAccountRepository Account { get; }
+        void Save();
+    }
+}
