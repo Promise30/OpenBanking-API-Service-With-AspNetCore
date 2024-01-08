6071b63336b9eeb965a5335fa34ae235dd44456d
Promise30 <promiseadeagbo@gmail.com>
2024-01-08 21:30:28 +0100

    task: initial implementation of the repository manager interface

---
+++ OpenBanking API Service/Infrastructures/Implementation/RepositoryManager.cs
@@ -0,0 +1,23 @@
+﻿using OpenBanking_API_Service.Data;
+using OpenBanking_API_Service.Infrastructures.Interface;
+
+namespace OpenBanking_API_Service.Infrastructures.Implementation
+{
+    public sealed class RepositoryManager : IRepositoryManager
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly Lazy<IAccountRepository> _accountRepository;
+        public RepositoryManager(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            _accountRepository = new Lazy<IAccountRepository>(() => new AccountRepository(applicationDbContext));
+
+        }
+        public IAccountRepository Account => _accountRepository.Value;
+
+        public void Save()
+        {
+            _applicationDbContext.SaveChanges();
+        }
+    }
+}
