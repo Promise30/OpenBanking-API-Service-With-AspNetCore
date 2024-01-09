d83222edb822d673d2e5afbec6a04aef32507bb9
Promise30 <promiseadeagbo@gmail.com>
2024-01-09 18:32:33 +0100

    task: request dtos for Bank Account operations

---
+++ OpenBanking API Service/Dtos/AccountsDto/Requests/CreateBankAccountWithdrawal.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenBanking_API_Service.Dtos.AccountsDto.Requests
+{
+    public class CreateBankAccountWithdrawal
+    {
+        [Required]
+        public string AccountNumber { get; set; }
+        [Required]
+        [Range(100, 1000000, ErrorMessage = "Amount withdrawal is between 100 - 1000000")]
+        public double Amount { get; set; }
+        [Required]
+        [Range(1000, 9999, ErrorMessage = "Pin must be of 4 digits.")]
+        public int Pin { get; set; }
+
+    }
+}
