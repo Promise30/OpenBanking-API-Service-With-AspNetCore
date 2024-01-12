69d901422c94dc11325418fec3e3defef64d287f
Promise30 <promiseadeagbo@gmail.com>
2024-01-12 18:48:23 +0100

    authenticatication response dtos

---
+++ OpenBanking API Service/Dtos/AuthenticationDtos/Responses/UserResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenBanking_API_Service.Dtos.AuthenticationDtos.Responses
+{
+    public class UserResponseDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
