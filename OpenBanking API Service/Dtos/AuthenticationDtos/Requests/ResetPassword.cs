ecc8da1e01b80feee3067ed2742e147cb3a7f9b7
Promise30 <promiseadeagbo@gmail.com>
2024-01-12 18:48:06 +0100

    authentication requests dto

---
+++ OpenBanking API Service/Dtos/AuthenticationDtos/Requests/ResetPassword.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenBanking_API_Service.Dtos.AuthenticationDtos.Requests
+{
+    public class ResetPassword
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "The password does not match.")]
+        public string ConfirmPassword { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ OpenBanking API Service/Dtos/AuthenticationDtos/Requests/TwoFactorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenBanking_API_Service.Dtos.AuthenticationDtos.Requests
+{
+    public class TwoFactorModel
+    {
+        public string OTP { get; set; }
+        public string Email { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
