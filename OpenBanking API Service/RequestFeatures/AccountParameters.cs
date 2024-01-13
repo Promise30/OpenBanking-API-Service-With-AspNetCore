5ae594a1386335bd2f95b7e7943a36ecdf8e4ea4
Promise30 <promiseadeagbo@gmail.com>
2024-01-13 01:23:43 +0100

    task: initial model definition for pagination and filtering

---
+++ OpenBanking API Service/RequestFeatures/AccountParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenBanking_API_Service.RequestFeatures
+{
+    public class AccountParameters : RequestParameters
+    {
+
+
+    }
+}
---
+++ OpenBanking API Service/RequestFeatures/AccountTransactionParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenBanking_API_Service.RequestFeatures
+{
+    public class AccountTransactionParameters : RequestParameters
+    {
+        public uint MinAmount { get; set; }
+        public uint MaxAmount { get; set; } = int.MaxValue;
+
+        public bool ValidAmountRange => MaxAmount > MinAmount;
+        //public string? SearchTerm { get; set; }
+
+
+        public AccountTransactionParameters() => OrderBy = "transactionDate";
+    }
+}
