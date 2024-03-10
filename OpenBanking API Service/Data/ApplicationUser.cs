238411da38891b20350bb2f403586197b27fc2a8
Promise30 <promiseadeagbo@gmail.com>
2024-03-10 12:23:49 +0100

    Added refresh token related properties

--- OpenBanking API Service/Data/ApplicationUser.cs
+++ OpenBanking API Service/Data/ApplicationUser.cs
@@ -6,6 +6,8 @@ namespace OpenBanking_API_Service.Data
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryDate { get; set; }
         public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.UtcNow;
         public DateTimeOffset ModifiedDate { get; set; } = DateTimeOffset.UtcNow;
         public virtual BankAccount BankAccount { get; set; }
