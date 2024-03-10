b9968feb811e15ee14200d24e53c33939e56ce3b
Promise30 <promiseadeagbo@gmail.com>
2024-03-10 12:24:48 +0100

    Defined enumerations for BankAccount model

---
+++ OpenBanking API Service/Domain/Enums/MaritalStatus.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.ComponentModel;
+
+namespace OpenBanking_API_Service.Domain.Enums
+{
+    public enum MaritalStatus
+    {
+        [Description("Single")]
+        OptionOne = 1,
+
+        [Description("Married")]
+        OptionTwo = 2,
+
+        [Description("Divorced")]
+        OptionThree = 3,
+
+
+    }
+
+}
