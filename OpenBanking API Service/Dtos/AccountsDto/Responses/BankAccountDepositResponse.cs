cdffa0a962d65e80a08b88b57ddd0df63b223201
Promise30 <promiseadeagbo@gmail.com>
2024-03-11 12:26:19 +0100

    Defined Dto models for BankAccount response operations

--- OpenBanking API Service/Dtos/AccountsDto/Responses/BankAccountDepositResponse.cs
+++ OpenBanking API Service/Dtos/AccountsDto/Responses/BankAccountDepositResponse.cs
@@ -3,7 +3,7 @@
     public class BankAccountDepositResponse
     {
         public Guid Id { get; set; }
-        public string AccountNumber { get; set; }
+        public string AccountNumber { get; set; } = string.Empty;
         public double Amount { get; set; }
         public double AccountBalance { get; set; }
         public Guid AccountId { get; set; }
--- OpenBanking API Service/Dtos/AccountsDto/Responses/BankAccountTransferResponse.cs
+++ OpenBanking API Service/Dtos/AccountsDto/Responses/BankAccountTransferResponse.cs
@@ -2,6 +2,7 @@
 {
     public class BankAccountTransferResponse
     {
+        public Guid Id { get; set; }
         public string SourceAccount { get; set; }
         public double Amount { get; set; }
         public double AccountBalance { get; set; }
