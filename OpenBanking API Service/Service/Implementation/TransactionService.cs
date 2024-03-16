58a6ddd4807d38acafc03c42e60790952e5f4e32
Promise30 <promiseadeagbo@gmail.com>
2024-03-16 12:38:56 +0100

    Modified existing response representation to reflect changes made to APIResponse class

--- OpenBanking API Service/Service/Implementation/TransactionService.cs
+++ OpenBanking API Service/Service/Implementation/TransactionService.cs
@@ -27,7 +27,7 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var bankAccount = await _repositoryManager.Account.GetBankAccountAsync(accountId, trackChanges);
                 if (bankAccount == null)
                 {
-                    return (APIResponse<IEnumerable<BankAccountDepositResponse>>.Create(HttpStatusCode.NotFound, "Bank account does not exist.", null), metaData: null);
+                    return (APIResponse<IEnumerable<BankAccountDepositResponse>>.Create(HttpStatusCode.NotFound, null, "Bank account does not exist."), metaData: new MetaData());
                 }
                 if (!accountTransactionParameters.ValidAmountRange)
                 {
@@ -36,12 +36,12 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var depositsWithMetaData = await _repositoryManager.BankDeposit.GetBankAccountDepositsAsync(accountId, accountTransactionParameters, trackChanges);
                 var depositsDto = _mapper.Map<IEnumerable<BankAccountDepositResponse>>(depositsWithMetaData);
 
-                return (APIResponse<IEnumerable<BankAccountDepositResponse>>.Create(HttpStatusCode.OK, "Request successful.", depositsDto), metaData: depositsWithMetaData.MetaData);
+                return (APIResponse<IEnumerable<BankAccountDepositResponse>>.Create(HttpStatusCode.OK, depositsDto, null), metaData: depositsWithMetaData.MetaData);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Something went wrong in the {nameof(GetBankAccountDepositsAsync)} service method {ex}");
-                return (APIResponse<IEnumerable<BankAccountDepositResponse>>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null), metaData: null);
+                return (APIResponse<IEnumerable<BankAccountDepositResponse>>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error"), metaData: new MetaData());
 
             }
         }
@@ -52,23 +52,23 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var account = await _repositoryManager.Account.GetBankAccountAsync(accountId, trackChanges);
                 if (account == null)
                 {
-                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.NotFound, "Account does not exist", null);
+                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.NotFound, null, "Account does not exist");
                 }
                 var bankDepositDb = await _repositoryManager.BankDeposit.GetBankAccountDepositAsync(accountId, id, trackChanges);
                 if (bankDepositDb == null)
                 {
-                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.NotFound, "Record does not exist", null);
+                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.NotFound, null, "Record does not exist");
                 }
 
                 var bankDeposit = _mapper.Map<BankAccountDepositResponse>(bankDepositDb);
-                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.OK, "Request successful", bankDeposit);
+                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.OK, bankDeposit, null);
 
             }
             catch (Exception ex)
             {
 
                 _logger.LogError($"Something went wrong in the {nameof(GetBankAccountDepositAsync)} service method {ex}");
-                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null);
+                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error");
             }
         }
 
@@ -79,11 +79,11 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var account = await _repositoryManager.Account.GetBankAccountByAccountNumberAsync(createBankAccountDeposit.AccountNumber, trackChanges: true);
                 if (account == null)
                 {
-                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.NotFound, "Account does not exist.", null);
+                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.NotFound, null, "Account does not exist.");
                 }
                 if (account.Pin != createBankAccountDeposit.Pin)
                 {
-                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.BadRequest, "Invalid Pin", null);
+                    return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.BadRequest, null, "Invalid Pin");
                 }
 
                 account.AccountBalance += createBankAccountDeposit.Amount;
@@ -103,13 +103,13 @@ namespace OpenBanking_API_Service.Service.Implementation
                 await _repositoryManager.SaveAsync();
 
 
-                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.Created, "Funds deposited successfully.", response);
+                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.Created, response, null);
             }
             catch (Exception ex)
             {
 
                 _logger.LogError($"Something went wrong in the {nameof(CreateBankAccountDepositAsync)} service method {ex}");
-                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null);
+                return APIResponse<BankAccountDepositResponse>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error");
             }
         }
 
@@ -121,7 +121,7 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var bankAccount = await _repositoryManager.Account.GetBankAccountAsync(accountId, trackChanges);
                 if (bankAccount == null)
                 {
-                    return (APIResponse<IEnumerable<BankAccountWithdrawalResponse>>.Create(HttpStatusCode.NotFound, "Bank account does not exist.", null), metaData: null);
+                    return (APIResponse<IEnumerable<BankAccountWithdrawalResponse>>.Create(HttpStatusCode.NotFound, null, "Bank account does not exist."), metaData: new MetaData());
                 }
                 if (!accountTransactionParameters.ValidAmountRange)
                 {
@@ -131,12 +131,12 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var withdrawalsWithMetaData = await _repositoryManager.BankWithdrawal.GetBankAccountWithdrawalsAsync(accountId, accountTransactionParameters, trackChanges);
                 var withdrawalsDto = _mapper.Map<IEnumerable<BankAccountWithdrawalResponse>>(withdrawalsWithMetaData);
 
-                return (APIResponse<IEnumerable<BankAccountWithdrawalResponse>>.Create(HttpStatusCode.OK, "Request successful.", withdrawalsDto), metaData: withdrawalsWithMetaData.MetaData);
+                return (APIResponse<IEnumerable<BankAccountWithdrawalResponse>>.Create(HttpStatusCode.OK, withdrawalsDto, null), metaData: withdrawalsWithMetaData.MetaData);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Something went wrong in the {nameof(GetBankAccountWithdrawalsAsync)} service method {ex}");
-                return (APIResponse<IEnumerable<BankAccountWithdrawalResponse>>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null), metaData: null);
+                return (APIResponse<IEnumerable<BankAccountWithdrawalResponse>>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error"), metaData: new MetaData());
 
             }
         }
@@ -148,23 +148,23 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var account = await _repositoryManager.Account.GetBankAccountAsync(accountId, trackChanges);
                 if (account == null)
                 {
-                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.NotFound, "Account does not exist", null);
+                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.NotFound, null, "Account does not exist");
                 }
                 var bankWithdrawalDb = await _repositoryManager.BankWithdrawal.GetBankAccountWithdrawalAsync(accountId, id, trackChanges);
                 if (bankWithdrawalDb == null)
                 {
-                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.NotFound, "Record does not exist", null);
+                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.NotFound, null, "Record does not exist");
                 }
 
                 var bankWithdrawal = _mapper.Map<BankAccountWithdrawalResponse>(bankWithdrawalDb);
-                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.OK, "Request successful", bankWithdrawal);
+                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.OK, bankWithdrawal, null);
 
             }
             catch (Exception ex)
             {
 
                 _logger.LogError($"Something went wrong in the {nameof(GetBankAccountWithdrawalsAsync)} service method {ex}");
-                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null);
+                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error");
             }
         }
 
@@ -175,15 +175,15 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var account = await _repositoryManager.Account.GetBankAccountByAccountNumberAsync(createBankAccountWithdrawal.AccountNumber, trackChanges: true);
                 if (account == null)
                 {
-                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.NotFound, "Account does not exist.", null);
+                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.NotFound, null, "Account does not exist.");
                 }
                 if (account.Pin != createBankAccountWithdrawal.Pin)
                 {
-                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.BadRequest, "Invalid Pin", null);
+                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.BadRequest, null, "Invalid Pin");
                 }
                 if (account.AccountBalance < createBankAccountWithdrawal.Amount)
                 {
-                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.BadRequest, "Insufficient account balance", null);
+                    return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.BadRequest, null, "Insufficient account balance");
                 }
 
                 account.AccountBalance -= createBankAccountWithdrawal.Amount;
@@ -203,13 +203,13 @@ namespace OpenBanking_API_Service.Service.Implementation
                 await _repositoryManager.SaveAsync();
 
 
-                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.Created, "Funds withdrawn successfully.", response);
+                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.Created, response, null);
             }
             catch (Exception ex)
             {
 
                 _logger.LogError($"Something went wrong in the {nameof(CreateBankAccountWithdrawalAsync)} service method {ex}");
-                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null);
+                return APIResponse<BankAccountWithdrawalResponse>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error");
             }
         }
 
@@ -220,7 +220,7 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var bankAccount = await _repositoryManager.Account.GetBankAccountAsync(accountId, trackChanges);
                 if (bankAccount == null)
                 {
-                    return (APIResponse<IEnumerable<BankAccountTransferResponse>>.Create(HttpStatusCode.NotFound, "Bank account does not exist.", null), metaData: null);
+                    return (APIResponse<IEnumerable<BankAccountTransferResponse>>.Create(HttpStatusCode.NotFound, null, "Bank account does not exist."), metaData: new MetaData());
                 }
                 if (!accountTransactionParameters.ValidAmountRange)
                 {
@@ -229,12 +229,12 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var transfersWithMetaData = await _repositoryManager.BankTransfer.GetBankAccountTransfersAsync(accountId, accountTransactionParameters, trackChanges);
                 var transfersDto = _mapper.Map<IEnumerable<BankAccountTransferResponse>>(transfersWithMetaData);
 
-                return (APIResponse<IEnumerable<BankAccountTransferResponse>>.Create(HttpStatusCode.OK, "Request successful.", transfersDto), metaData: transfersWithMetaData.MetaData);
+                return (APIResponse<IEnumerable<BankAccountTransferResponse>>.Create(HttpStatusCode.OK, transfersDto, null), metaData: transfersWithMetaData.MetaData);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Something went wrong in the {nameof(GetBankAccountTransfersAsync)} service method {ex}");
-                return (APIResponse<IEnumerable<BankAccountTransferResponse>>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null), metaData: null);
+                return (APIResponse<IEnumerable<BankAccountTransferResponse>>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error"), metaData: new MetaData());
 
             }
         }
@@ -246,23 +246,23 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var account = await _repositoryManager.Account.GetBankAccountAsync(accountId, trackChanges);
                 if (account == null)
                 {
-                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.NotFound, "Account does not exist", null);
+                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.NotFound, null, "Account does not exist");
                 }
                 var bankTransferDb = await _repositoryManager.BankTransfer.GetBankAccountTransferAsync(accountId, id, trackChanges);
                 if (bankTransferDb == null)
                 {
-                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.NotFound, "Record does not exist", null);
+                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.NotFound, null, "Record does not exist");
                 }
 
                 var bankTransfer = _mapper.Map<BankAccountTransferResponse>(bankTransferDb);
-                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.OK, "Request successful", bankTransfer);
+                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.OK, bankTransfer, null);
 
             }
             catch (Exception ex)
             {
 
                 _logger.LogError($"Something went wrong in the {nameof(GetBankAccountTransferAsync)} service method {ex}");
-                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null);
+                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error");
             }
         }
 
@@ -274,15 +274,15 @@ namespace OpenBanking_API_Service.Service.Implementation
                 var destinationAccount = await _repositoryManager.Account.GetBankAccountByAccountNumberAsync(createBankAccountTransfer.DestinationAccount, trackChanges: true);
                 if (sourceAccount == null || destinationAccount == null)
                 {
-                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.NotFound, "Account does not exist.", null);
+                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.NotFound, null, "Account does not exist.");
                 }
                 if (sourceAccount.Pin != createBankAccountTransfer.Pin)
                 {
-                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.BadRequest, "Invalid Pin", null);
+                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.BadRequest, null, "Invalid Pin");
                 }
                 if (sourceAccount.AccountBalance < createBankAccountTransfer.Amount)
                 {
-                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.BadRequest, "Insufficient account balance.", null);
+                    return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.BadRequest, null, "Insufficient account balance.");
                 }
 
                 sourceAccount.AccountBalance -= createBankAccountTransfer.Amount;
@@ -300,15 +300,7 @@ namespace OpenBanking_API_Service.Service.Implementation
 
                 };
 
-                //var receipientDeposit = new BankDeposit
-                //{
-                //    AccountId = destinationAccount.BankAccountId,
-                //    AccountBalance = destinationAccount.AccountBalance,
-                //    AccountNumber = destinationAccount.AccountNumber,
-                //    TransactionDate = DateTimeOffset.Now,
-                //    Amount = createBankAccountTransfer.Amount,
 
-                //};
 
                 var receipientTransfer = new BankTransfer
                 {
@@ -329,13 +321,13 @@ namespace OpenBanking_API_Service.Service.Implementation
                 await _repositoryManager.SaveAsync();
 
 
-                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.Created, "Funds transferred successfully.", response);
+                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.Created, response, null);
             }
             catch (Exception ex)
             {
 
                 _logger.LogError($"Something went wrong in the {nameof(CreateBankAccountTransferAsync)} service method {ex}");
-                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.InternalServerError, "Internal Server error", null);
+                return APIResponse<BankAccountTransferResponse>.Create(HttpStatusCode.InternalServerError, null, "Internal Server error");
             }
         }
     }
