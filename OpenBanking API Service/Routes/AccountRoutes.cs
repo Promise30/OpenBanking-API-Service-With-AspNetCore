9f34c9d89b037ab081d3768a6d5d11a0491a0198
Promise30 <promiseadeagbo@gmail.com>
2024-03-16 12:35:34 +0100

    Defined const properties to store controller routes

---
+++ OpenBanking API Service/Routes/AccountRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenBanking_API_Service.Routes
+{
+    public class AccountRoutes
+    {
+        public const string CreateAccount = "create-account";
+        public const string GetAccount = "get-account";
+        public const string GetAccounts = "get-accounts";
+        public const string DeleteAccount = "delete-account";
+        public const string UpdateAccount = "update-account";
+
+    }
+}
