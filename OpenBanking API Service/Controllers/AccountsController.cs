ce27a4df9f94590524e76699a7c6384b8a24f914
Promise30 <promiseadeagbo@gmail.com>
2024-03-18 12:42:21 +0100

    Added XML comments on controller methods for API Documentation

--- OpenBanking API Service/Controllers/AccountsController.cs
+++ OpenBanking API Service/Controllers/AccountsController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using OpenBanking_API_Service.Dtos.AccountsDto.Requests;
+using OpenBanking_API_Service.Dtos.AccountsDto.Responses;
 using OpenBanking_API_Service.RequestFeatures;
+using OpenBanking_API_Service.Routes;
 using OpenBanking_API_Service.Service.Interface;
 using System.Net;
 using System.Text.Json;
@@ -18,17 +21,37 @@ namespace OpenBanking_API_Service.Controllers
         {
             _accountService = accountService;
         }
-
+        /// <summary>
+        /// Gets the list of all registered accounts
+        /// </summary>
+        /// <param name="accountParameters"></param>
+        /// <returns>The registered accounts list</returns>
+        /// <response code="200">Returns the list of all accounts</response>
+        /// <response code="400">If something went wrong</response>
         [HttpGet]
-        //[Authorize(Roles = "Admin")]
+        [ProducesResponseType(typeof(IEnumerable<BankAccountDto>), 200, "application/json")]
+        [ProducesResponseType(400)]
+        //[Authorize]
         public async Task<IActionResult> GetBankAccounts([FromQuery] AccountParameters accountParameters)
         {
             var pagedResult = await _accountService.GetAllBankAccountsAsync(accountParameters, trackChanges: false);
 
             Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(pagedResult.metaData));
-            return pagedResult.Item1.StatusCode == HttpStatusCode.OK ? Ok(pagedResult.Item1) : StatusCode((int)pagedResult.Item1.StatusCode, pagedResult.Item1);
+            return pagedResult.accounts.StatusCode == HttpStatusCode.OK ? Ok(pagedResult.accounts) : StatusCode((int)pagedResult.accounts.StatusCode, pagedResult.accounts);
+
         }
 
+        /// <summary>
+        /// Gets a single bank account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>The single bank account</returns>
+        /// <response code="200">Returns the bank account</response>
+        /// <response code="400">If bank account does not exist</response>
+        /// <response code="500">If there was a server error</response>     
+        [ProducesResponseType(typeof(BankAccountDto), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
         [HttpGet("{accountId:guid}")]
         public async Task<IActionResult> GetBankAccount(Guid accountId)
         {
@@ -38,17 +61,91 @@ namespace OpenBanking_API_Service.Controllers
         }
 
 
-
+        /// <summary>
+        /// creates a new bank account
+        /// </summary>
+        /// <param name="createBankAccountDto"></param>
+        /// <returns>The newly created account</returns>
+        /// <remarks>
+        /// Sample request:
+        ///     
+        ///         POST /create-account
+        ///         {
+        ///             "firstName": "Clark",
+        ///             "lastName": "Kent",
+        ///             "middleName": "Claire",
+        ///             "dateOfBirth": "2000-01-11T12:45:57.671Z",
+        ///             "gender": "Female",
+        ///             "residentCountry": "Nigeria",
+        ///             "residentAddress": "Ikeja, Lagos",
+        ///             "residentPostalCode": "100213",
+        ///             "city": "Lagos",
+        ///             "state": "Lagos",
+        ///             "birthCountry": "Niger",
+        ///             "maritalStatus": 1,
+        ///             "accountType": 1,
+        ///             "pin": 1234
+        ///         }
+        /// </remarks>
+        /// <response code="201">Returns the newly created bank account</response>
+        /// <response code="400">If the model is invalid</response>
+        /// <response code="401">If the user is not authenticated</response>
+        /// <response code="500">If there was a server error</response>     
+        [ProducesResponseType(typeof(BankAccountDto), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(500)]
         [Authorize]
-        [HttpPost("create-account")]
+        [HttpPost]
+        [Route(AccountRoutes.CreateAccount)]
         public async Task<IActionResult> CreateBankAccount(CreateBankAccount createBankAccountDto)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, "Validation Failed", ModelState));
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
             }
             var response = await _accountService.CreateBankAccountAsync(createBankAccountDto);
             return response.StatusCode == HttpStatusCode.OK ? Ok(response) : StatusCode((int)response.StatusCode, response);
         }
+        /// <summary>
+        /// Update user account details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="patchDoc"></param>
+        /// <returns>Returns no content</returns>
+        [HttpPatch("update-account/{id:guid}")]
+        [Authorize]
+        public async Task<IActionResult> PartiallyUpdateBankAccountDetail(Guid id, [FromBody] JsonPatchDocument<BankAccountForUpdateDto> patchDoc)
+        {
+            if (patchDoc is null)
+            {
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
+            }
+            var result = await _accountService.GetBankAccountForPatch(id, trackChanges: true);
+            patchDoc.ApplyTo(result.bankAccountToPatch);
+            _accountService.SaveChangesForPatch(result.bankAccountToPatch, result.bankAccountEntity);
+
+            return NoContent();
+
+        }
+
+        /// <summary>
+        /// Delete a bank account
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns no content</returns>
+        /// <response code="204">Account was deleted successful</response>
+        /// <response code="404">If bank account does not exist</response>
+        /// <response code="500">If there was a server error</response>
+        [HttpDelete("delete-account/{id:guid}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> DeleteBankAccount(Guid id)
+        {
+            var result = await _accountService.DeleteBankAccountAsync(id, trackChanges: false);
+            return result.StatusCode == HttpStatusCode.OK ? Ok(result) : StatusCode((int)result.StatusCode, result);
+        }
+
     }
 }
--- OpenBanking API Service/Controllers/AuthenticationController.cs
+++ OpenBanking API Service/Controllers/AuthenticationController.cs
@@ -2,128 +2,220 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using OpenBanking_API_Service.Dtos.AuthenticationDtos.Requests;
+using OpenBanking_API_Service.Dtos.AuthenticationDtos.Responses;
+using OpenBanking_API_Service.Routes;
 using OpenBanking_API_Service.Service.Constants;
 using OpenBanking_API_Service.Service.Interface;
 using System.Net;
+using System.Net.Mime;
 
 namespace OpenBanking_API_Service.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
         private readonly IUserService _userService;
         private readonly IEmailService _emailService;
-        private readonly IBankAccountService _accountService;
-
-        public AuthenticationController(IUserService userService,
-                                        IEmailService emailService,
-                                        IBankAccountService accountService)
+        public AuthenticationController(IUserService userService, IEmailService emailService)
         {
             _userService = userService;
             _emailService = emailService;
-            _accountService = accountService;
         }
+        /// <summary>
+        /// register a new user to use the service
+        /// </summary>
+        /// <param name="registerUserDto"></param>
+        /// <returns>An email confirmation link</returns>
+        /// <response code="200">Account was  created successful</response>
+
+        /// <response code="400">If model is invalid or username already exists</response>
+        /// <response code="500">If something went wrong with the server</response>
         [AllowAnonymous]
-        [HttpPost("register")]
+        [Consumes(MediaTypeNames.Application.Json)]
+
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        [HttpPost]
+        [Route(AuthenticationRoutes.RegisterUser)]
         public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, "Validation Failed", ModelState));
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
             }
-            var tokenResponse = await _userService.RegisterNewUser(registerUserDto);
 
-            if (tokenResponse.Data != null)
+            var response = await _userService.RegisterNewUser(registerUserDto);
+
+            if (response.StatusCode == HttpStatusCode.OK)
             {
-                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token = tokenResponse.Data, email = registerUserDto.Email }, Request.Scheme);
+
+                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token = response.Data, email = registerUserDto.Email }, Request.Scheme);
                 var message = new EmailMessage(to: new string[] { registerUserDto.Email }, subject: "Email Confirmation", body: confirmationLink);
                 var responseMessage = _emailService.SendEmail(message);
-                return StatusCode(StatusCodes.Status200OK, tokenResponse);
-            }
-            return StatusCode(StatusCodes.Status400BadRequest, tokenResponse);
-        }
 
-        [HttpGet("confirm-email")]
+                return StatusCode(StatusCodes.Status200OK, response);
+            }
+            return StatusCode((int)response.StatusCode, response);
+        }
+        /// <summary>
+        /// confirm a newly registered user email
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="email"></param>
+        /// <returns>a success confirmation message</returns>
+        /// <response code="200">If email confirmation was successful</response>
+        [ProducesResponseType(200)]
+        [HttpGet]
+        [Route(AuthenticationRoutes.ConfirmUserEmail)]
         public async Task<IActionResult> ConfirmEmail(string token, string email)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
+            }
             var result = await _userService.UserEmailConfirmation(token, email);
             return result.StatusCode == HttpStatusCode.OK ? Ok(result) : BadRequest(result);
 
         }
 
-        [HttpPost("login")]
+
+
+        /// <summary>
+        /// login a registered user using only username and password
+        /// </summary>
+        /// <param name="loginDto"></param>
+        /// <returns>An access and refresh token for user without 2FA and OTP for user with 2FA</returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(TokenDto), 200)]
+        [ProducesResponseType(400)]
+
+        [Route(AuthenticationRoutes.LoginUser)]
+
         public async Task<IActionResult> Login(LoginDto loginDto)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, "Validation Failed", ModelState));
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
             }
 
             var result = await _userService.LoginUserAsync(loginDto);
-
-            if (result.Data != null)
-            {
-                var otpToken = result.Data.Token;
-                var message = new EmailMessage(new string[] { loginDto.Email }, "OTP Confirmation Code", otpToken);
-                _emailService.SendEmail(message);
-                return StatusCode(StatusCodes.Status200OK, result);
-
-            }
-            return StatusCode(StatusCodes.Status400BadRequest, result);
+            return result.StatusCode == HttpStatusCode.OK ? Ok(result) : StatusCode((int)result.StatusCode, result);
         }
 
-        [HttpPost("login-2FA")]
+        /// <summary>
+        /// login a registered user using OTP code
+        /// </summary>
+        /// <param name="twoFactorModel"></param>
+        /// <returns>An access and refresh token</returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(TokenDto), 200)]
+        [ProducesResponseType(400)]
+        //[ValidateAntiForgeryToken]
+        [Route(AuthenticationRoutes.LoginUserWith2FA)]
         public async Task<IActionResult> Login2FA(TwoFactorModel twoFactorModel)
         {
-            var result = await _userService.LoginUserWithTwoFactorEnabled(twoFactorModel);
-            if (result.Data != null)
+            if (!ModelState.IsValid)
             {
-                return StatusCode(StatusCodes.Status200OK, result);
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
             }
-            return BadRequest(result);
+            var result = await _userService.LoginTwoFactorUserAsync(twoFactorModel);
+            return result.StatusCode == HttpStatusCode.OK ? Ok(result) : StatusCode((int)result.StatusCode, result);
         }
-        [HttpGet("reset-password")]
+
+        /// <summary>
+        /// allows a user to make a request to reset their current password
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route(AuthenticationRoutes.ResetUserPassword)]
         public async Task<IActionResult> ResetPassword(string token, string email)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
+            }
             var model = new ResetPassword { Token = token, Email = email };
             return Ok(model);
         }
 
-
-        [HttpPost("reset-password")]
+        /// <summary>
+        /// allows a user to provide credentials to reset their current password
+        /// </summary>
+        /// <param name="resetPassword"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route(AuthenticationRoutes.ResetUserPassword)]
         public async Task<IActionResult> ResetPassword(ResetPassword resetPassword)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, "Validation Failed", ModelState));
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
             }
             var result = await _userService.PasswordResetAsync(resetPassword);
             return result.StatusCode == HttpStatusCode.OK ? Ok(result) : BadRequest(result);
         }
-        [HttpPost("forgot-password")]
+
+        /// <summary>
+        /// Sends a request to reset the user's password based on their email address.
+        /// </summary>
+        /// <param name="forgotPassword"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route(AuthenticationRoutes.ForgotPassword)]
         public async Task<IActionResult> ForgotPassword(ForgotPassword forgotPassword)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
+            }
             var result = await _userService.ForgotPasswordRequest(forgotPassword);
-            if (result.Data != null)
+            if (result.StatusCode == HttpStatusCode.OK)
             {
                 var resetPasswordLink = Url.Action(nameof(ResetPassword), "Authentication", new { token = result.Data, email = forgotPassword.Email }, Request.Scheme);
                 var message = new EmailMessage(to: new string[] { forgotPassword.Email }, subject: "Password Reset Confirmation Link", body: resetPasswordLink);
                 var responseMessage = _emailService.SendEmail(message);
                 return StatusCode(StatusCodes.Status200OK, result);
             }
-
             return StatusCode(StatusCodes.Status400BadRequest, result);
-
-
         }
 
+        /// <summary>
+        /// allows a user to get a new access and refresh token 
+        /// </summary>
+        /// <param name="tokenDto"></param>
+        /// <returns>A new access and refresh token</returns>
+        /// <response code="200">If a new access and refresh token was generated</response>
+        /// <response code="400">If token provided is invalid</response>
+        [HttpPost]
+        [Route(AuthenticationRoutes.RefreshUserToken)]
+        [ProducesResponseType(typeof(TokenDto), 200)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Refresh(TokenDto tokenDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(APIResponse<ModelStateDictionary>.Create(HttpStatusCode.BadRequest, ModelState, "Validation Failed"));
+            }
+            var result = await _userService.RefreshToken(tokenDto);
+            return result.StatusCode == HttpStatusCode.OK ? Ok(result) : StatusCode((int)result.StatusCode, result);
+        }
+        [HttpGet]
+        [Route(AuthenticationRoutes.LogOutUser)]
+        public async Task<IActionResult> LogOut()
+        {
+            var result = await _userService.LogoutUserAsync();
+            return result.StatusCode == HttpStatusCode.OK ? Ok(result) : StatusCode((int)result.StatusCode, result);
+        }
 
-
-
-
+        [HttpGet("registered-users")]
+        public async Task<IActionResult> GetRegisteredUsers()
+        {
+            var result = await _userService.GetUsers();
+            return Ok(result);
+        }
     }
-
-
-
 }
